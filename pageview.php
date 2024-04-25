63aab5c99e55fb7c364a58a3963a76400db22636
Abhi Mondal <85216013+AbhiMondal20@users.noreply.github.com>
2024-04-25 20:10:14 +0530

    mpdfvire
    final for Design
    its done

--- pageview.php
+++ pageview.php
@@ -1,7 +1,35 @@
 <?php
+    require_once __DIR__ . '/vendor/autoload.php'; 
+    $mpdf = new \Mpdf\Mpdf();
+    // $mpdf->useOddEven = 1;
 
-require_once __DIR__ . '/vendor/autoload.php';
+    // $mpdf->AddPageByArray(array(
+    //     'resetpagenum' => '1',
+    //     'pagenumstyle' => 'i',
+    //     'suppress' => 'on',
+    // ));
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+    $mpdf->SetHeader('Document Title|Center Text|{PAGENO}');
+    $mpdf->AliasNbPages('[pagetotal]');
+    $mpdf->PageNo();
+    $mpdf->WriteHTML('
+        There are [pagetotal] pages in this document
+    ');
+
+    $mpdf->WriteHTML('<htmlpageheader name="myHeader2">
+    <div style="text-align: center; font-weight: bold;">
+        My document
+    </div>
+    </htmlpageheader>
+    <p>Text of introduction...
+    <p>Text of main book...');
+    
+    $mpdf->WriteHTML('<indexentry content="Buffalo" />Your text which refers to a buffalo, which
+    you would like to see in the Index
+    ...rest of document <pagebreak topmargin="100"/> <h2>Index</h2>');
+
+    $mpdf->aliasNbPg = '[pagetotal]';
+    $mpdf->WriteHTML('Some text...');
+    $mpdf->WriteHTML('<columnbreak />');
+    $mpdf->WriteHTML('Next column...');
+    $mpdf->Output();
